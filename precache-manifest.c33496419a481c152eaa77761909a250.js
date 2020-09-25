da70fbb36da90ad361a89acd1dd1bc980529a990
Christine Go <eg160@duke.edu>
2020-09-25 21:55:20 +0900

    Updates

---
+++ precache-manifest.c33496419a481c152eaa77761909a250.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6889ff76e9251b3447f94eeee58bceb1",
+    "url": "/takka/index.html"
+  },
+  {
+    "revision": "532b482db6ae718f7e5f",
+    "url": "/takka/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "51ad8bf853b643fb0b84",
+    "url": "/takka/static/js/2.0df98d72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/takka/static/js/2.0df98d72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "532b482db6ae718f7e5f",
+    "url": "/takka/static/js/main.f78126f8.chunk.js"
+  },
+  {
+    "revision": "477dbd59748557dac371",
+    "url": "/takka/static/js/runtime-main.ca3bbf15.js"
+  }
+]);
